9f3ec2c53c825a17c72ef6970471a4abb6be16a7
kvloewenstein <Kvloewenstein@yahoo.com>
2025-11-04 19:49:20 -0600

    updated LoginPage.jsx and SignupPage.jsx

--- skin-sync-springboot-app/src/main/java/com/SkinSync/backend/repository/ProductRepository.java
+++ skin-sync-springboot-app/src/main/java/com/SkinSync/backend/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package com.SkinSync.backend.repository;
 import com.SkinSync.backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Filter products by skinType and skinCondition
+    List<Product> findBySkinTypeAndSkinCondition(String skinType, String skinCondition);
 }
